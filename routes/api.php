8ec012dfd04d45833ce0734f32966964f00e59ca
Ashahab007 <ashahab007@gmail.com>
2025-11-02 03:15:43 +0600

    final backed up

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ Route::get('/user', function (Request $request) {
 
 // 15. here we will define our api routes later
 
-// 16. now we are going to create resource controller by "php artisan make:controller PostController --resource" which will create a file name PostController in Controllers folder which will create all the crud methods.
+// 16. now we are going to create resource controller by "php artisan make:controller PostController --resource" which will create a file name "PostController" in app => Http => Controllers => PostController where we will create all the crud methods.
 
 // 17. create Route then to see all routes run "php artisan route:list" u will see created all routes for PostController in terminal
 Route::resource('posts', PostController::class);
